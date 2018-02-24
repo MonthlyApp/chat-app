3aacd4f811a04f5451ba7d1fc17cdb6f29a93de7
Tamachandesu <taturo.0324@gmail.com>
2018-02-25 03:06:33 +0900

    Edit messages_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,16 @@
 class MessagesController < ApplicationController
   def index
-    @message = "kennyasan"
+    @message = Message.new
+    @messages = Message.all
   end
 
-  def show
+  def create
+      @message = Message.new(message_params)
+      @message.save
+      redirect_to action: :index
+  end
 
+  def message_params
+    params.require(:message).permit(:text, :user_id)
   end
 end
