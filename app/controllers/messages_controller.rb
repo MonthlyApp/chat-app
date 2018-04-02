44f4a2ddcf782b89dc699f96d05c451558a9f377
Tamachandesu <taturo.0324@gmail.com>
2018-04-03 07:14:31 +0900

    Test

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,15 +2,28 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = Message.all
+    #render json: @messages
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @messages }
+    end
+    #format.json {render json: @messages, :status => created}
+
+    # render json: @messages, status: :created, location: @message
   end
 
   def create
-      @message = Message.new(message_params)
-      @message.save
-      redirect_to action: :index
+    @message = Message.create(message_params)
+    respond_to do |format|
+      format.html
+      format.json { render :json => @messages }
+    end
   end
 
+
+
   def message_params
-    params.require(:message).permit(:text, :user_id)
+    params.require(:message).permit(:user_id, :text)
   end
 end
